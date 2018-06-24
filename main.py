7756c9964c7bf802841ca445f765958915712400
RNatvik <ronatvik@gmail.com>
2018-06-24 02:02:01 +0200

    Added commandline arguments for input and output file, optional arguments for DTE weight and show_path

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from PIL import Image
-from datetime import datetime
 import time
+import argparse
 from maze import Maze
+from node import Node
 import simpleSolver
 
 
-def saveImage(im, result):
-    print("Saving Image")
+def saveImage(im, result, output_file):
+    print("\nSaving Image")
     im = im.convert('RGB')
     impixels = im.load()
 
@@ -31,19 +32,20 @@ def saveImage(im, result):
             for y in range(min(a[0], b[0]), max(a[0], b[0]) + 1):
                 impixels[y, a[1]] = px
 
-    # TODO, make output image dependent on arguments
-    destination = "mazes/result/"
-    dateTime = datetime.now()
-    filename = str(dateTime.date()) + " " + str(dateTime.time()) + ".png"
-    im.save(destination + filename)
+    im.save(output_file)
+    outputTree = output_file.split("/")
+    filename = outputTree[len(outputTree) - 1]
+    destination = ""
+    for i in range(len(outputTree) - 1):
+        destination += outputTree[i] + "/"
+
     print("Image saved in " + destination + " as " + filename)
 
 
-def main():
+def main(input_file, output_file, show_path):
     print("Opening image...")
 
-    # TODO, make image dependent on argument
-    image = Image.open("mazes/perfect2k.png")
+    image = Image.open(input_file)
 
     print("Analyzing image and creating nodes...")
     startTime = time.time()
@@ -59,20 +61,49 @@ def main():
 
     endTime = time.time()
     totalTime = endTime - startTime
-    print("Solve time:", totalTime)
-    #
-    # # TODO, make this segment dependent on arguments
-    # print("\nPath:")
-    # for node in path:
-    #     print(node.Position)
 
+    if show_path is True:
+        print("\nPath:")
+        for node in path:
+            print(node.Position)
+
+    print("Solve time:", totalTime)
     print("\nVisited nodes:", visitedNodes)
     print("Considered nodes:", consideredNodes)
-    print("Path pixel length:", maze.end.distanceToStart)
+    print("Path pixel length:", int(maze.end.distanceToStart))
     print("Path node length:", len(path))
+    print("DTE Weight:", Node.weight)
 
-    saveImage(image, path)
+    saveImage(image, path, output_file)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input",
+                        help="the input image file of the maze")
+
+    parser.add_argument("output",
+                        help="the output image file")
+
+    parser.add_argument("--showPath",
+                        help="Print the path in terminal (default is False)",
+                        choices=["true", "false"])
+
+    parser.add_argument("--weight",
+                        type=int,
+                        help="A number indicating the weight of distance to end when prioritizing nodes (default is 1)",
+                        choices=[0, 1, 2, 3, 4, 5])
+
+    args = parser.parse_args()
+    input_file = args.input
+    output_file = args.output
+    show_path = args.showPath
+    weight = args.weight
+    if show_path is None:
+        show_path = False
+    if weight is None:
+        weight = 1
+
+    Node.weight = weight
+    main(input_file, output_file, show_path)
