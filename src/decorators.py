72e9da21f8ed001f87cb0f07121371610727f6bd
RNatvik <ronatvik@gmail.com>
2018-07-22 16:53:00 +0200

    Refactored in generator.py Added timed_with_rv in decorators for methods with their own return values

--- src/decorators.py
+++ src/decorators.py
@@ -5,6 +5,16 @@ from time import time
 def timed(func):
     def timed_method(*args, **kwargs):
         start_time = time()
+        func(*args, **kwargs)
+        end_time = time()
+        total_time = end_time - start_time
+        return total_time
+    return timed_method
+
+
+def timed_with_rv(func):
+    def timed_method(*args, **kwargs):
+        start_time = time()
         rv = func(*args, **kwargs)
         end_time = time()
         total_time = end_time - start_time
